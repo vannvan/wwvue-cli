3b4f255557ac3df52e43b3fb4722ef4aba299841
shiwan <shiwanwan@thinkjoy.cn>
2019-05-29 09:54:51 +0800

    init cli

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
-# wwvue-cli
+## about
 
-> A Vue.js project
+集成了`sass` 、`axios` 、`vuex`等vue项目必要插件，同时完善了这些插件的使用配置，对vuex进行了模块化管理，同时集成了部分常用的方法，包含自定义指令的运用场景参考、全局混入的运用场景参考、接口封装等实际项目的标准化模块。
 
-## Build Setup
+## install
 
-``` bash
-# install dependencies
-npm install
+> npm i wwvue-cli -g
 
-# serve with hot reload at localhost:8080
-npm run dev
+## use
 
-# build for production with minification
-npm run build
+> wwvue init project-name
 
-# build for production and view the bundle analyzer report
-npm run build --report
-```
+## catelog
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+![](https://github.com/vannvan/wvue-cli/blob/master/src/assets/images/Catalog.png?raw=true)
+
+
+
+## Command line deployment
+
+运用命令行完成每次版本的更新,将本地更新推送至远程仓库。
+
+> npm run push  --init
+
+## Command line packaging
+
+运用命令函完成每次需要交付给运维的项目压缩文件。
+
+> npm run pack
+
+## 更多人性化功能还在不断迭代更新！
+
---
+++ bin/index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const program = require('commander');
+const download = require('download-git-repo');
+const handlebars = require('handlebars');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const chalk = require('chalk');
+const symbols = require('log-symbols');
+const shell = require("shelljs");
+const getGitUser = require("../lib/git-user");
+
+const author = getGitUser()
+
+var setDefault = ''
+if(author) {
+    setDefault = author
+}
+const promptList = [
+        {
+          name: 'description',
+          message: 'Project description:',
+          default: 'A Vue.js project'
+        },
+        {
+          name: 'author',
+          message: 'Project author:',
+          default: setDefault
+        }
+      ]
+program.version('1.1.0', '-v, --version')
+  .command('init <name>')
+  .action((name) => {
+    if(!fs.existsSync(name)){
+      inquirer.prompt(promptList).then((answers) => {
+        const spinner = ora('正在下载模板...');
+        spinner.start();
+        download('https://github.com:vannvan/wvue-cli#master', name, {clone: true}, (err) => {
+          if(err){
+            spinner.fail();
+            console.log(symbols.error, chalk.red(err));
+          }else{
+            spinner.succeed();
+            const fileName = `${name}/package.json`;
+            const meta = {
+              "name":name,
+              "description": answers.description,
+              "author": answers.author
+            }
+            if(fs.existsSync(fileName)){
+              const content = fs.readFileSync(fileName).toString();
+              const result = handlebars.compile(content)(meta);
+              // console.log(result)
+              fs.writeFileSync(fileName, result);
+            }
+            console.log(symbols.success, chalk.green('项目初始化完成'));
+          }
+        })
+      })
+    }else{
+      console.log(symbols.error, chalk.red('项目已存在'));
+    }
+  })
+program.parse(process.argv);
