cbc5446a8cd9d278eb6a52b01987cc7fb88710d2
vannvan <862670198@qq.com>
2020-12-03 09:35:20 +0800

    fix:接口引入方式错误修正

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,9 @@
 import { post } from './http'
 
 export default {
-    getUserList: p => post('/userList', p), //获取用户列表 
+  getUserList: (p) =>
+    post(
+      'https://www.easy-mock.com/mock/5fa4043f60b9b97b2c399398/api/userinfo',
+      p
+    ), //获取用户列表
 }
