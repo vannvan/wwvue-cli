9cd9651a027da2f11f4614afa172ec02a6a4d713
vannvan <862670198@qq.com>
2020-12-17 22:15:05 +0800

    引入sentry

--- src/main.js
+++ src/main.js
@@ -4,9 +4,26 @@ import router from './router/index'
 import store from './store/index'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
-
 import './wui-theme/src/index.scss'
 
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://2d29ca1348e64cb380c09d9c8dbe84c3@o473545.ingest.sentry.io/5508525",
+    integrations: [
+        new VueIntegration({
+            Vue,
+            tracing: true,
+        }),
+        new Integrations.BrowserTracing(),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
 
 import WUI from './packages'
 Vue.use(WUI)
