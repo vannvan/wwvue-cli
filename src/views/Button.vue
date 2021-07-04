15643180285c31ec66f02c177db3f8f5b754f72f
vannvan <862670198@qq.com>
2021-07-04 16:44:33 +0800

    tmp

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,5 +1,6 @@
+/* eslint-disable new-cap */ /* eslint-disable new-cap */
 <template>
-  <div>
+  <div id="Button">
     <div class="button-wrap">
       <w-button type="primary" size="small"> 按钮 </w-button>
       <w-button type="primary"> 按钮 </w-button>
@@ -18,15 +19,55 @@
       <w-button type="info" plain> 按钮 </w-button>
       <w-button type="error" plain> 按钮 </w-button>
     </div>
+    <button @click="handleRecord()">录制</button>
+    <button @click="handlePlay()">回放</button>
+    <div id="radio" style="width: 500px; height: 300px; border: 1px solid #ccc"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, Ref, onMounted } from 'vue'
+import * as rrweb from 'rrweb'
+import 'rrweb-player/dist/style.css'
+import RrwebPlayer from 'rrweb-player'
 
 export default defineComponent({
   setup() {
-    return {}
+    const events: Ref = ref(null)
+
+    onMounted(() => {
+      // 每 10 秒调用一次 save 方法，避免请求过多
+      //   setInterval(save, 10 * 1000)
+      // setTimeout(() => {}, 1200)
+    })
+
+    const handleRecord = () => {
+      events.value = []
+      rrweb.record({
+        emit(event: any) {
+          // 将 event 存入 events 数组中
+          events.value.push(event)
+        }
+      })
+    }
+
+    const handlePlay = () => {
+      console.log('handlePlay')
+      console.log(events.value)
+      const values: number[] = events.value
+      // eslint-disable-next-line no-new
+      new RrwebPlayer({
+        target: document.getElementById('radio') as HTMLElement,
+        data: {
+          values
+        }
+      })
+    }
+
+    return {
+      handleRecord,
+      handlePlay
+    }
   }
 })
 </script>
@@ -40,3 +81,5 @@ export default defineComponent({
   }
 }
 </style>
+
+function onMounted(arg0: () => void) { throw new Error('Function not implemented.') }
